c9835610faa27e314d7dde63f1aedbd3a6d0e5d5
PHANNA11 <phaphanna2018@gmail.com>
2022-07-30 16:42:44 +0700

    insert and read

---
+++ lib/View/main_screen.dart
@@ -0,0 +1,119 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:sqlite1/Model/user_model.dart';
+import 'package:sqlite1/connection/database_connection.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController controller = TextEditingController();
+  late DatabaseConnection db;
+  Future<List<User>>? listUser;
+  Future<List<User>> getList() async {
+    return await db.getUser();
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    db = DatabaseConnection();
+    db.initializeUserDB().whenComplete(() async {
+      setState(() {
+        listUser = db.getUser();
+        print(listUser!.then((value) => value.first.name.toString()));
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.amberAccent,
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                  hintText: 'Enter name', border: OutlineInputBorder()),
+            ),
+          ),
+          Container(
+              height: 400,
+              width: double.infinity,
+              child: FutureBuilder(
+                future: listUser,
+                builder: (context, AsyncSnapshot<List<User>> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  //  else if (snapshot.hasError) {
+                  //   return Center(
+                  //     child: Icon(
+                  //       Icons.info,
+                  //       color: Colors.red,
+                  //       size: 28,
+                  //     ),
+                  //   );
+                  // } else {
+                  //   var item = snapshot.data ?? <User>[];
+                  //   ListView.builder(
+                  //     itemCount: item.length,
+                  //     itemBuilder: (context, index) {
+                  //       return ListTile(
+                  //         title: Text(item[index].name),
+                  //       );
+                  //     },
+                  //   );
+                  // }
+                  return snapshot.hasError
+                      ? const Center(
+                          child: Icon(
+                            Icons.info,
+                            color: Colors.red,
+                            size: 28,
+                          ),
+                        )
+                      : ListView.builder(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            var item = snapshot.data![index];
+                            return ListTile(
+                              title: Text(item.name),
+                            );
+                          },
+                        );
+                },
+              )),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await DatabaseConnection()
+              .insetUser(User(id: Random().nextInt(200), name: controller.text))
+              .whenComplete(() {
+            print('insert succes');
+          });
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.done),
+      ),
+    );
+  }
+}
---
+++ lib/connection/database_connection.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqlite_api.dart';
+import 'package:path/path.dart';
+import '../Model/user_model.dart';
+
+String table = 'user';
+
+class DatabaseConnection {
+  Future<Database> initializeUserDB() async {
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    String appDocPath = appDocDir.path;
+    String path = await getDatabasesPath();
+    return openDatabase(join(path, 'userData.db'),
+        onCreate: (db, version) async {
+      await db.execute('CREATE TABLE $table(id INTEGER PRIMIRY KEY,name TEXT)');
+    }, version: 1);
+  }
+
+  Future<void> insetUser(User user) async {
+    final db = await initializeUserDB();
+    await db.insert(table, user.toMap());
+    print('funtion insert');
+  }
+
+  Future<List<User>> getUser() async {
+    final db = await initializeUserDB();
+    List<Map<String, dynamic>> qresult = await db.query(table);
+    print('lll');
+    return qresult.map((e) => User.fromMap(e)).toList();
+  }
+}
